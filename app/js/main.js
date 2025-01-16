7831f5e829bbd551e25b81847e5e737d3d6518ac
Hannibal860 <skorokhodeugene@gmail.com>
2025-01-16 14:30:49 -0700

    task 3 is almost done, need help with slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,17 @@
 $(function () {
   var mixer = mixitup('.gallery__content-list');
+
 })
+
+
+$('.hero__background').slick({
+  dots: false,
+  arrows: false,
+  infinite: true,
+  slidesToShow: 1,
+  autoplay: true,
+  speed: 100,
+  fade: true,
+  cssEase: 'linear'
+});
+
