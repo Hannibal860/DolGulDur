a63827707f2dd8afc6973bac9f3936e3d1bf564f
Hannibal860 <skorokhodeugene@gmail.com>
2025-02-09 10:37:51 -0700

    first step adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,20 @@
+(() => {
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+
+  menuBtnRef.addEventListener("click", () => {
+    const expanded =
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+    mobileMenuRef.classList.toggle("is-open");
+  });
+})();
+
+
+
 $(function () {
   var mixer = mixitup('.gallery__content-list');
 
@@ -32,5 +49,8 @@ $('.hero__background').slick({
     refs.modal.classList.toggle("is-hidden");
   }
 })();
+
+
+
 
 
